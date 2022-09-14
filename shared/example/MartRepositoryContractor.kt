5ee83f568075d35d18c028f269cd5ba070032de6
Nithichai Thepmong <nithichai.thepmong@scbtechx.io>
2022-09-14 22:25:08 +0700

    Delete example file

--- shared/example/MartRepositoryContractor.kt
+++
@@ -1,7 +0,0 @@
-package th.co.robinhood.mart.repository
-
-
-interface MartRepositoryContractor {
-    
-    fun getMartFav(request: MartFavRequest): Single<MartFav>
-}
--- shared/example/PromotionRepositoryContractor.kt
+++
@@ -1,7 +0,0 @@
-package th.co.robinhood.promotion.repository
-
-
-interface PromotionRepositoryContractor {
-    
-    fun getPromotionJaa(request: PromotionJaaRequest): Single<PromotionJaa>
-}
